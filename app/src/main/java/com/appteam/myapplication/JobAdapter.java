4aa8da2a3d7ad787144bb8e6e7251981db9a562d
LamGitKraken <lamitptit99@gmail.com>
2021-04-03 02:25:42 +0700

    stupid commit

---
+++ app/src/main/java/com/appteam/myapplication/JobAdapter.java
@@ -0,0 +1,63 @@
+package com.appteam.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.appteam.myapplication.databinding.ItemJobBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobAdapter extends RecyclerView.Adapter<JobAdapter.JobViewHolder> {
+    private List<Job> listJob;
+    ItemJobBinding binding;
+
+    public JobAdapter(List<Job> listJob) {
+        this.listJob = listJob;
+    }
+
+    class JobViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private int position;
+        public JobViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+        public void bind(Job job,int position){
+            this.position = position;
+            binding.btnDelete.setOnClickListener(this);
+//            binding.image.setImageURI(job.getImage());
+            binding.name.setText(job.getName());
+            binding.salary.setText(String.valueOf(job.getSalary()));
+            binding.dateCreate.setText(job.getDate_created().toString());
+            binding.activated.setText(job.isActivated()?"Activated":"Inactivated");
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeJob(position);
+        }
+    }
+    @NonNull
+    @Override
+    public JobViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding = ItemJobBinding.inflate(LayoutInflater.from(parent.getContext()), parent,false);
+        return new JobViewHolder(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull JobViewHolder holder, int position) {
+        holder.bind(listJob.get(position),position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listJob.size();
+    }
+    public void removeJob(int position){
+        listJob.remove(position);
+        notifyItemChanged(position);
+    }
+}
--- app/src/main/java/com/appteam/myapplication/MainActivity.java
+++ app/src/main/java/com/appteam/myapplication/MainActivity.java
@@ -1,14 +1,40 @@
 package com.appteam.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.appteam.myapplication.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ActivityMainBinding binding;
+    JobAdapter jobAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setUpRecyclerView();
+        binding.btnAdd.setOnClickListener(this);
+    }
+    private void setUpRecyclerView(){
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("a",1.0 ,Calendar.getInstance().getTime(),true));
+        jobAdapter = new JobAdapter(list);
+
+        binding.recyclerJob.setAdapter(jobAdapter);
+        binding.recyclerJob.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        binding.recyclerJob.setHasFixedSize(true);
+    }
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, AddJobActivity.class));
     }
 }
