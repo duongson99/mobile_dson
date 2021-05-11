f10cdc904dd17f3ab90e46e4cc6d3fe4b27e9a06
1amToxic <lamitptit99@gmail.com>
2021-05-11 21:16:26 +0700

    complete problem 2

--- app/src/main/java/com/appteam/myapplication/MainActivity.java
+++ app/src/main/java/com/appteam/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.appteam.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
@@ -8,33 +11,22 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.appteam.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    JobAdapter jobAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setUpRecyclerView();
-        binding.btnAdd.setOnClickListener(this);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
     }
-    private void setUpRecyclerView(){
-        List<Job> list = new ArrayList<>();
-        list.add(new Job("a",1.0 ,Calendar.getInstance().getTime(),true));
-        jobAdapter = new JobAdapter(list);
 
-        binding.recyclerJob.setAdapter(jobAdapter);
-        binding.recyclerJob.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        binding.recyclerJob.setHasFixedSize(true);
-    }
-    @Override
-    public void onClick(View v) {
-        startActivity(new Intent(this, AddJobActivity.class));
-    }
 }
